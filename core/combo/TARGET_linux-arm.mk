d4655996155770903635dd3b03056337e3bbc28a
synergydev <synergy@spentlemon.com>
2013-07-10 20:09:35 +0000

    Revert "linux-arm: leave TARGET_GCC_VERSION up to build script"
    This reverts commit d3a93d66ec8ec486acb4d7f233bc5f2352442296.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -34,12 +34,14 @@ ifeq ($(strip $(TARGET_ARCH_VARIANT)),)
 TARGET_ARCH_VARIANT := armv5te
 endif
 
-ifneq ($(TARGET_BUILD_TYPE),development)
-    ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
-    TARGET_GCC_VERSION := 4.8
+ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
+    ifeq ($(TARGET_BUILD_TYPE),development)
+        TARGET_GCC_VERSION := inline
     else
-    TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
+        TARGET_GCC_VERSION := 4.8
     endif
+else
+TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
 
 TARGET_ARCH_SPECIFIC_MAKEFILE := $(BUILD_COMBOS)/arch/$(TARGET_ARCH)/$(TARGET_ARCH_VARIANT).mk
