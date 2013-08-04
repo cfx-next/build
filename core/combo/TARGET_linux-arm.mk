a3cf7b8945c8fade6f7c6de55b30c21613212b52
synergydev <synergye@codefi.re>
2013-08-04 12:00:20 -0700

    Improve how we set LTO
    Summary:
    This introduces the following improvements:
      - Can be globally disabled with DISABLE_BUILD_LTO
      - Can be locally disabled with LOCAL_NO_LTO_SUPPORT.
      - Host modules now build without LTO by default.
      - Modules built with clang no longer need -fno-lto specified locally (LTO disabled here automatically)
      - Modules built with clang no longer have -fno-toplevel-reorder unnecessarily set.
    
    Test Plan: Win
    
    Reviewers: randall.rushing, Mustaavalkosta, michyprima, Roach, synergy
    
    Reviewed By: synergy
    
    Differential Revision: http://review.codefirex.com/D293

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -82,8 +82,6 @@ TARGET_arm_CFLAGS :=    -fgcse-after-reload \
                         -fvect-cost-model \
                         -fomit-frame-pointer \
                         -foptimize-sincos \
-                        -flto \
-                        -fno-toplevel-reorder \
                         -fstrict-aliasing \
                         -Wstrict-aliasing=3 \
                         -Werror=strict-aliasing
@@ -96,8 +94,6 @@ TARGET_thumb_CFLAGS :=  -mthumb \
                         -Werror=strict-aliasing \
                         -fgcse-after-reload \
                         -fsched-spec-load \
-                        -flto \
-                        -fno-toplevel-reorder \
                         -funroll-loops \
                         -foptimize-sincos \
                         -fvect-cost-model \
@@ -164,10 +160,7 @@ TARGET_GLOBAL_LDFLAGS += \
 
 TARGET_GLOBAL_CFLAGS += -mthumb-interwork
 
-TARGET_GLOBAL_CPPFLAGS += \
-                          -fvisibility-inlines-hidden \
-                          -flto \
-                          -fno-toplevel-reorder
+TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 
 # More flags/options can be added here
 TARGET_RELEASE_CFLAGS += \
@@ -193,6 +186,14 @@ ifneq ($(wildcard $(TARGET_CC)),)
 TARGET_LIBGCC := $(shell $(TARGET_CC) $(TARGET_GLOBAL_CFLAGS) -print-libgcc-file-name)
 target_libgcov := $(shell $(TARGET_CC) $(TARGET_GLOBAL_CFLAGS) \
         -print-file-name=libgcov.a)
+endif
+
+# Define LTO (Link Time Optimization options
+
+ifeq ($(strip $(DISABLE_BUILD_LTO)),)
+  # Disable global LTO if DISABLE_BUILD_LTO is set.
+  TARGET_LTO_CFLAGS := -flto \
+                       -fno-toplevel-reorder
 endif
 
 # Define FDO (Feedback Directed Optimization) options.
