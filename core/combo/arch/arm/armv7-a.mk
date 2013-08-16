60bcec98ec4f2ae577d4a0bafda53b8a7b55973c
synergydev <synergye@codefi.re>
2013-08-16 10:29:08 -0700

    armv7-a: make fewer assumptions
    Summary: fixes armv7-a build
    
    Test Plan: win
    
    Reviewers: randall.rushing, synergy
    
    Reviewed By: synergy
    
    Differential Revision: http://review.codefirex.com/D359

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -1,62 +1,73 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv7-a architecture and higher
 #
-ARCH_ARM_HAVE_THUMB_SUPPORT     := true
-ARCH_ARM_HAVE_FAST_INTERWORKING := true
-ARCH_ARM_HAVE_64BIT_DATA        := true
-ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
-ARCH_ARM_HAVE_CLZ               := true
-ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
-ifeq ($(TARGET_ARCH_VARIANT_FPU), neon)
-ARCH_ARM_HAVE_VFP_D32           := true
-ARCH_ARM_HAVE_NEON              := true
-endif
-
-mcpu-arg = $(shell sed 's/^-mcpu=//' <<< "$(call cc-option,-mcpu=$(1),-mcpu=$(2))")
-
-ifeq ($(TARGET_CPU_VARIANT), cortex-a15)
-TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a15,cortex-a9)
-ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
-ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
-#ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
-endif
-ifeq ($(TARGET_CPU_VARIANT), krait)
-TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
-ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
-ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
-ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
-endif
-ifeq ($(TARGET_CPU_VARIANT), cortex-a9)
-TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
-ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
-ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
-ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
-endif
-ifeq ($(TARGET_CPU_VARIANT), cortex-a8)
-TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a8,)
-ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
-ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
-ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
-endif
 
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
 arch_variant_cflags := \
-    -march=armv7-a \
-    -mfloat-abi=softfp
-
-ifneq ($(TARGET_ARCH_VARIANT_FPU), neon)
-arch_variant_cflags := \
+    -mfloat-abi=softfp \
     -mfpu=vfpv3-d16
+
+ifneq ($(TARGET_CPU_VARIANT), cortex-a15)
+  arch_variant_cflags += -march=armv7-a
 endif
 
-ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
-arch_variant_ldflags := \
-        -Wl,--no-fix-cortex-a8
-else
 arch_variant_ldflags := \
         -Wl,--fix-cortex-a8
+
+#
+# Cpu variant specific flags
+#
+ifeq ($(TARGET_CPU_VARIANT), cortex-a15)
+  ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS     := true
+  ARCH_ARM_NEON_MEMSET_DIVIDER            := 132
 endif
+
+ifeq ($(TARGET_CPU_VARIANT), krait)
+  ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS     := true
+  ARCH_ARM_NEON_MEMSET_DIVIDER            := 132
+  ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER  := 224
+  TARGET_USE_KRAIT_BIONIC_OPTIMIZATION    := true
+  TARGET_USE_KRAIT_PLD_SET                := true
+  TARGET_KRAIT_BIONIC_PLDOFFS             := 10
+  TARGET_KRAIT_BIONIC_PLDTHRESH           := 10
+  TARGET_KRAIT_BIONIC_BBTHRESH            := 64
+  TARGET_KRAIT_BIONIC_PLDSIZE             := 64
+endif
+
+ifeq ($(TARGET_CPU_VARIANT), cortex-a9)
+  ifeq ($(call is-vendor-board-platform,QCOM),true)
+    TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+    TARGET_USE_SCORPION_PLD_SET             := true
+    TARGET_USE_SCORPION_PLD_SET             := true
+    TARGET_SCORPION_BIONIC_PLDOFFS          := 6
+    TARGET_SCORPION_BIONIC_PLDSIZE          := 128
+  endif
+  ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS     := true
+  ARCH_ARM_NEON_MEMSET_DIVIDER            := 132
+  ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER  := 224
+endif
+
+ifeq ($(TARGET_CPU_VARIANT), cortex-a8)
+  ifeq ($(call is-vendor-board-platform,QCOM),true)
+    TARGET_USE_SPARROW_BIONIC_OPTIMIZATION  := true
+  endif
+  ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS     := true
+  ARCH_ARM_NEON_MEMSET_DIVIDER            := 132
+  ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER  := 224
+endif
+
+# Neither "krait" or "generic" are valid -mcpu values.
+# Set a valid alternative for krait, and leave generic
+# without an argument.
+ifeq ($(filter generic krait, $(TARGET_CPU_VARIANT)),)
+  arch_variant_cflags += -mcpu=$(TARGET_CPU_VARIANT)
+else
+  ifeq ($(TARGET_CPU_VARIANT), krait)
+    arch_variant_cflags += -mcpu=cortex-a9
+  endif
+endif
+
